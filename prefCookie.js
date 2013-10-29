bd6e6df0751a50678633ffc0900aa9bddd467019
Audun <cyborg.projects@gmail.com>
2013-10-28 21:46:57 -0400

    Update prefCookie.js
    readCookie now returns a list instead of a string.

--- prefCookie.js
+++ prefCookie.js
@@ -1,5 +1,5 @@
 /*
-This script should run the first time a user runs the site or app.
+This script will run the first time a user runs the site or app.
 It will read their selection of preferred categories, then pass
 them to a jQuery function that creates a cookie. 
 
@@ -7,13 +7,13 @@ The script contains the following functions:
 
 readDoc(): Reads the HTML for selections made by the user.
 readCookie(): Reads pre-existing cookies, calls createCookie if
-none is found
+none is found. Returns a list
 createCookie(): Creates cookie based on input from readDoc()
 
 readCookie will be the first to be called. It runs when a button
 with id 'next' is clicked. If readCookie does not find a cookie, 
 or the cookie is empty, it calls createCookie(readDoc()) and then
-returns that cookie.
+reads outputs that cookie.
 
 createCookie() requires an argument that is a list of categories.
 This will be given by readDoc()
@@ -22,15 +22,13 @@ This will be given by readDoc()
 
 
 $(document).ready(function(){
-
-	//Eventlistener
+	//Click eventlistener
 	$("#next").click(function(){
 	readCookie();
 	console.log($.cookie("prefs"));
 	});
 
 	readDoc = function(){
-	  //Reads checkboxes, takes their values, and returns a list of them
 		var lst = $('input:checked'); 				//Return all currently selected elements
 		var retLst = [];
 		for(var i = 0; i < lst.length; i++){
@@ -46,7 +44,6 @@ $(document).ready(function(){
 	readCookie = function(){
 
 		//Create new cookie if empty or non-existant
-		//Changes cookie if selection has changed
 		if($.cookie("prefs") === undefined){
 			createCookie(readDoc());
 			console.log("Cookie 'prefs' not found, created new cookie");
@@ -55,6 +52,7 @@ $(document).ready(function(){
 			$.removeCookie("prefs");
 			createCookie(readDoc());
 		}
-		return $.cookie("prefs");	
+
+		return $.cookie("prefs").split(",")
 	}
 })
