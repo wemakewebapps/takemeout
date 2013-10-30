5eb1c32c47caf376ba4f10f5f7ffc8d614316e6a
Audun <cyborg.projects@gmail.com>
2013-10-30 14:26:02 -0400

    Update prefCookie.js
    Fixed a bug where the cookie would delete itself upon refresh if the form in the document was left empty.
    Added a function checkTheBoxes() that reads any existing cookie and checks the checkboxes of all categories that were stored in it.

--- prefCookie.js
+++ prefCookie.js
@@ -9,6 +9,9 @@ readDoc(): Reads the HTML for selections made by the user.
 readCookie(): Reads pre-existing cookies, calls createCookie if
 none is found. Returns a list
 createCookie(): Creates cookie based on input from readDoc()
+checkTheBoxes(): Reads the content of the cookie, then checks
+the checkboxes for the categories the user has previously
+spcified
 
 readCookie will be the first to be called. It runs when a button
 with id 'next' is clicked. If readCookie does not find a cookie, 
@@ -18,41 +21,85 @@ reads outputs that cookie.
 createCookie() requires an argument that is a list of categories.
 This will be given by readDoc()
 
+checkTheBoxes should require no other call than the one made at
+the bottom of this script.
+
 */
 
 
-$(document).ready(function(){
+$(document).ready(function()
+{
+	readDoc = function()
+	{
+		var lst = $('input:checked'); 				//Return all currently selected elements
+		var retLst = [];
+		for(var i = 0; i < lst.length; i++);
+		{
+			retLst[i] = lst[i].value;					//Get the values from the form
+		}
+			return retLst;
+	}
+
+	createCookie = function(content)
+	{
+		$.cookie("prefs", content);
+	}
+
+	readCookie = function()
+	{
+		//Check if input is different than current cookie
+		if(readDoc() != $.cookie("prefs"))
+		{
+			if(readDoc().length == 0)
+			{									//Do nothing if document contains no info
+				$.cookie("prefs");
+			}
+			else
+			{
+				console.log("Cookie changed");
+				$.removeCookie("prefs");		//Delete original cookie
+				createCookie(readDoc());		//Create new cookie
+			}
+		}
+
+		//Create new cookie if empty or non-existant
+		if($.cookie("prefs") === undefined)
+		{
+			createCookie(readDoc());
+			console.log("Cookie 'prefs' not found, created new cookie");
+		}
+		//Return cookie as list
+		return $.cookie("prefs").split(",");
+	}
+
+	checkTheBoxes = function()
+	{
+		var checkboxes = $('input');
+		var from_cookie = readCookie();
+
+		for(var i = 0; i < checkboxes.length; i++);
+		{
+			if (!checkboxes[i].checked);
+			{														//If any given input is not checked
+				for(var j = 0; j < from_cookie.length; j++);
+				{													//compare it to all elements in the cookie.
+					if(checkboxes[i].value === from_cookie[j]);
+					{												//Set input to checked if there is a match
+						checkboxes[i].checked = "checked";
+					}
+				}
+			}
+		}
+	}
+
 	//Click eventlistener
-	$("#next").click(function(){
+	$("#next").click(function()
+	{
 	readCookie();
 	console.log($.cookie("prefs"));
 	});
 
-	readDoc = function(){
-		var lst = $('input:checked'); 				//Return all currently selected elements
-		var retLst = [];
-		for(var i = 0; i < lst.length; i++){
-			retLst[i] = lst[i].value;					//Get the values
-		}
-		return retLst;
-	}
-
-	createCookie = function(content){
-		$.cookie("prefs", content);
-	}
-
-	readCookie = function(){
-
-		//Create new cookie if empty or non-existant
-		if($.cookie("prefs") === undefined){
-			createCookie(readDoc());
-			console.log("Cookie 'prefs' not found, created new cookie");
-		}
-		else if(readDoc() !== $.cookie("prefs")){
-			$.removeCookie("prefs");
-			createCookie(readDoc());
-		}
-
-		return $.cookie("prefs").split(",")
-	}
+	//Check the checkboxes of pre-existing preferences
+	checkTheBoxes();
+	
 })
